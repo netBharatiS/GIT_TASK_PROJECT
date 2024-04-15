b650ce418eb5786c7f0657616b34a54269bc9eef
net pra <xyz@domain.com>
2024-04-15 13:01:01 +0100

    improve with function call

--- helloWorld.py
+++ helloWorld.py
@@ -7,16 +7,20 @@ print(f"Hello! {name}")
 
 age = int(input("Enter your age:"))
 
-
-if age > 100 :
-  print("Sorry you are dead.")
-elif age >= 65:
-  print("Enjoy your retirement!")
-elif age >= 40:
-  print("You're over the hill.")
-elif age == 21:
-  print("Congrats on your 21st!")
-elif age < 13:
-  print("You qualify for kiddie discount.")
-else:
-  print("Age is but a number.")
+# Function to determine age bracket and print message to user
+def determine_age(age):
+  if age > 100 :
+    print("Sorry you are dead.")
+  elif age >= 65:
+    print("Enjoy your retirement!")
+  elif age >= 40:
+    print("You're over the hill.")
+  elif age == 21:
+    print("Congrats on your 21st!")
+  elif age < 13:
+    print("You qualify for kiddie discount.")
+  else:
+    print("Age is but a number.")
+  
+# Function call
+determine_age(age)
